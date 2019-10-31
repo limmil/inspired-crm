310d12e3ad423ceaee22a238352dddfd96f38be7
shartlej <shartlej@ecs.csus.edu>
2019-10-31 14:29:28 -0700

    Update Login.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -19,12 +19,16 @@ class Login extends Component {
       // If logged in and user navigates to Login page, should redirect them to dashboard
       if (this.props.auth.isAuthenticated) {
          this.props.history.push("/dashboard");
+         window.location.reload(false); 
+
       }
    }
 
    componentWillReceiveProps(nextProps) {
       if (nextProps.auth.isAuthenticated) {
          this.props.history.push("/dashboard");
+         window.location.reload(false); 
+
       }
 
       if (nextProps.errors) {
