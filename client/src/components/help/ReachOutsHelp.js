095419b4fab7953fa608d9571f99efd03ad7c812
JB <justinbrianeugenio@gmail.com>
2019-11-09 20:59:50 -0800

    Help modals.
    Added help modals for (?)'s.
    
    Commit by Justin.
    
    User story card #115(#121).

--- client/src/components/help/ReachOutsHelp.js
+++ client/src/components/help/ReachOutsHelp.js
@@ -5,13 +5,26 @@ class ReachOutsHelp extends Component {
    render() {
       return (
          <header>
-                <div class="modal-content">
-                    <h4><i class="material-icons medium">help</i> New Reach Outs Help</h4>
-                        <p>Select the contacts you have never spoken to about your business. If you select them here, you intend to reach out to them this week. Don’t know who you want to reach out to? Does choosing the people to reach out to stress you out or overwhelm you? Make it a challenge to contact anyone the system chooses no matter how intimidated you are to do so. This will help build your confidence and show you that you’re capable of pitching ANYONE!!</p>
-                </div>
-                <div class="modal-footer">
-                    <a href="#!" class="modal-close waves-effect waves-light btn">Ok</a>
-                </div>
+            <div class="modal-content">
+               <h4>
+                  <i class="material-icons small">help</i> New Reach Outs Help
+               </h4>
+               <p>
+                  Select the contacts you have never spoken to about your
+                  business. If you select them here, you intend to reach out to
+                  them this week. Don’t know who you want to reach out to? Does
+                  choosing the people to reach out to stress you out or
+                  overwhelm you? Make it a challenge to contact anyone the
+                  system chooses no matter how intimidated you are to do so.
+                  This will help build your confidence and show you that you’re
+                  capable of pitching ANYONE!!
+               </p>
+            </div>
+            <div class="modal-footer">
+               <a href="#!" class="modal-close waves-effect waves-light btn">
+                  Ok
+               </a>
+            </div>
          </header>
       );
    }
