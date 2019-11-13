ea26d9d6ff097b676d6a0fb873d3cba0f1ede6d1
Michael M <mmorrison@csus.edu>
2019-11-12 19:06:21 -0800

    Calendar pulls from DB, D&D in progress

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,36 +1,19 @@
-// ALTERNATE CALENDAR IMPORT IGNORE FOR NOW
-// import { Calendar, momentLocalizer } from "react-big-calendar";
-// import moment from './moment'
+// import React, { Component } from "react";
+// import axios from "axios";
+// import TableRow from "./TableRow";
+// import DashboardNavbar from "../dashboard/DashboardNavbar";
+// import DashboardFooter from "../dashboard/DashboardFooter";
+
+// import {
+//     Calendar,
+//     momentLocalizer,
+//   } from 'react-big-calendar';
+// import moment from "./moment";
+
+// import "./Calendar.css";
+// import "react-big-calendar/lib/css/react-big-calendar.css";
 
 // const localizer = momentLocalizer(moment)
-// const MyCalendar = props => (
-//     <div>
-//         <Calendar
-//         localizer={localizer}
-//         //   events={myEventsList}
-//         startAccessor="start"
-//         endAccessor="end"
-//         />
-//     </div>
-//     )
-// ALTERNATE CALENDAR IMPORT IGNORE FOR NOW
-
-import React, { Component } from "react";
-import axios from "axios";
-import TableRow from "./TableRow";
-import DashboardNavbar from "../dashboard/DashboardNavbar";
-import DashboardFooter from "../dashboard/DashboardFooter";
-
-import {
-    Calendar,
-    momentLocalizer,
-  } from 'react-big-calendar';
-import moment from "./moment";
-
-import "./Calendar.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-
-const localizer = momentLocalizer(moment)
  
 
 // METHOD 1
@@ -56,53 +39,211 @@ const localizer = momentLocalizer(moment)
 
 // METHOD 2
 /********************************************************* */
-class UserCalendar extends Component {
-  constructor(props) {
-     super(props);
-     this.state = { eventss: [] };
-  }
+// class UserCalendar extends Component {
+//   constructor(props) {
+//      super(props);
+//      this.state = { eventss: [] };
+//   }
 
-  componentDidMount() {
-     const user = {
-        email: localStorage.getItem("userEmail"),
-        tokenhash: localStorage.getItem("tokenHash")
-     };
-     console.log(user);
-     axios
-        .post("/api/events/get", user)
-        .then(response => {
-           console.log(response);
-           this.setState({ eventss: response.data });
-        })
-        .catch(function(error) {
-           console.log(error);
-        });
-  }
-/********************************************************* */
+//   componentDidMount() {
+//      const user = {
+//         email: localStorage.getItem("userEmail"),
+//         tokenhash: localStorage.getItem("tokenHash")
+//      };
+//      console.log(user);
+//      axios
+//         .post("/api/events/get", user)
+//         .then(response => {
+//            console.log(response);
+//            this.setState({ eventss: response.data });
+//         })
+//         .catch(function(error) {
+//            console.log(error);
+//         });
+//   }
+// /********************************************************* */
 
 
 
-  render() {
-    return (
-       <div>
-         <DashboardNavbar />
-         <div className="UserCalendar">
-            <Calendar
-               localizer={localizer}
-               defaultDate={new Date()}
-               defaultView="month"
-               events={this.state.eventss}
-               style={{ height: "100vh" }}
-            />
-         </div>
+//   render() {
+//     return (
+//        <div>
+//          <DashboardNavbar />
+//          <div className="UserCalendar">
+//             <Calendar
+//                localizer={localizer}
+//                defaultDate={new Date()}
+//                defaultView="month"
+//                events={this.state.eventss}
+//                style={{ height: "100vh" }}
+//             />
+//          </div>
 
          
         
+//       </div>
+//     );
+//   }
+// }
+// export default UserCalendar;
+
+
+
+
+
+
+
+
+
+
+
+
+//DRAG AND DROP PROTOTYPE You can try it, but  ***REQUIRES MORE EVENT SCHEMA WORK*** 
+import React, { Component } from "react";
+import axios from "axios";
+import TableRow from "./TableRow";
+import DashboardNavbar from "../dashboard/DashboardNavbar";
+import DashboardFooter from "../dashboard/DashboardFooter";
+import PropTypes from "prop-types";
+import { updateContact } from "../../actions/calendarActions";
+import {
+    Calendar,
+    momentLocalizer,
+  } from 'react-big-calendar';
+import moment from "./moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
+import "./Calendar.css";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+// const localizer = Calendar.momentLocalizer(moment);
+const localizer = momentLocalizer(moment)
+const DnDCalendar = withDragAndDrop(Calendar);
+
+class UserCalendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { eventsList: [] };
+ }
+
+ componentDidMount() {
+  const user = {
+     email: localStorage.getItem("userEmail"),
+     tokenhash: localStorage.getItem("tokenHash")
+  };
+  console.log(user);
+  axios
+     .post("/api/events/get", user)
+     .then(response => {
+        console.log(response);
+        this.setState({ eventsList: response.data });
+     })
+     .catch(function(error) {
+        console.log(error);
+     });
+ }
+
+  onEventResize = (type, { event, start, end, allDay }) => {
+    // axios
+    //  .post("/api/events/update", this)
+    //  .then(response => {
+    //     console.log(response);
+    //     this.setState({ eventsList: response.data });
+    //  })
+    //  .catch(function(error) {
+    //     console.log(error);
+    //  });
+
+    var newEvent = {
+                start: start,
+                end: end,
+                title: this.title,
+               }
+
+    this.props.updateCalendar(newEvent);
+
+    this.setState(state => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: state.events };
+    });
+  };
+
+  // onEventResize {
+  //   // axios
+  //   //  .post("/api/events/update", this)
+  //   //  .then(response => {
+  //   //     console.log(response);
+  //   //     this.setState({ eventsList: response.data });
+  //   //  })
+  //   //  .catch(function(error) {
+  //   //     console.log(error);
+  //   //  });
+
+  //   this.setState(state => {
+  //     state.events[0].start = start;
+  //     state.events[0].end = end;
+  //     return { events: state.events };
+  //   });
+  // };
+
+
+
+
+
+
+
+
+  onEventDrop = ({ event, start, end, allDay }) => {
+    // this.setState(state => {
+    //   state.events[0].start = start;
+    //   state.events[0].end = end;
+    //   return { events: state.events };
+    // });
+    console.log(start);
+  };
+
+  render() {
+    return (
+      <div>
+        <DashboardNavbar />
+        <div className="UserCalendar">
+        {/* <DnDCalendar */}
+        {/* replace the line below this with the line above to resume work on d&d */}
+         <Calendar
+           localizer={localizer}
+            defaultDate={new Date()}
+            defaultView="month"
+            events={this.state.eventsList}
+            onEventDrop={this.onEventDrop}
+            onEventResize={this.state.onEventResize}
+            resizable
+           style={{ height: "100vh" }}
+        />
       </div>
+
+    </div>
     );
   }
 }
 export default UserCalendar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
