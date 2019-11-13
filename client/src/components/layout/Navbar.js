0ecc81e225db1b310142582ce9f8fe10ccf8fddb
JB <justinbrian@users.noreply.github.com>
2019-11-12 21:11:09 -0800

    Update dashboard goal trackers.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,44 +2,53 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-   render() {
-      return (
-         <div>
-            <div className="navbar-fixed">
-               <nav className="z-depth-0"></nav>
-               <nav class="white" role="navigation">
-                  <div class="nav-wrapper container">
-                     <a id="logo-container" class="brand-logo">
-                        <Link to="/" onClick={this.forceUpdate}>
-                           <font color="#212121">TSG CRM</font>
-                        </Link>
-                     </a>
-                     <ul class="right hide-on-med-and-down">
-                        <li>
-                           <Link to="/login" class="waves-effect waves-light btn blue">
-                                 Log In
-                           </Link>
-                        </li>
-                     </ul>
+  render() {
+    return (
+      <div>
+        <div className="navbar-fixed">
+          <nav className="z-depth-0"></nav>
+          <nav class="white" role="navigation">
+            <div class="nav-wrapper container">
+              <a id="logo-container" class="brand-logo">
+                <Link to="/" onClick={this.forceUpdate}>
+                  <font color="#212121">TSG CRM</font>
+                </Link>
+              </a>
+              <ul class="right hide-on-med-and-down">
+                <li>
+                  <Link to="/login" class="waves-effect waves-light btn blue">
+                    Log In
+                  </Link>
+                </li>
+              </ul>
 
-                     <a
-                        data-target="nav-mobile"
-                        class="sidenav-trigger"
-                     >
-                        <i class="material-icons">menu</i>
-                     </a>
-                  </div>
-               </nav>
+              <a data-target="nav-mobile" class="sidenav-trigger">
+                <i class="material-icons">menu</i>
+              </a>
             </div>
+          </nav>
+        </div>
 
-            <ul id="nav-mobile" class="sidenav">
-               <li>
-                  <Link to="/login">Login</Link>
-               </li>
-            </ul>
-         </div>
-      );
-   }
+        <ul id="nav-mobile" class="sidenav">
+          <li>
+            <center>
+              <b>Welcome</b>
+            </center>
+          </li>
+          <li>
+            <Link to="/login">
+              <i class="material-icons">vpn_key</i>Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register">
+              <i class="material-icons">how_to_reg</i>Register
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
