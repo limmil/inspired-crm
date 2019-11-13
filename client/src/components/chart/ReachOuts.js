5d3f7d079cad7d369fa6df776256e0974054b305
JB <justinbrianeugenio@gmail.com>
2019-11-12 16:33:33 -0800

    #25(#120)
    Doughnut progress graph added on dashboard. Only front-end.
    
    Commit by Justin.
    
    User story card #25(#120).

---
+++ client/src/components/chart/ReachOuts.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class ReachOuts extends Component {
+  render() {
+    return (
+      <header>
+        <div class="chart-container">
+          <div class="doughnut-chart-container">
+            <canvas id="doughnut-chartcanvas-1"></canvas>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default ReachOuts;
